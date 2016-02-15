ac32d7e1c02094936455f9b4df4a013eb7d6095c
Tom White <tom@sixdozen.com>
2016-02-15 00:16:10 -0800

    gnt2image: changed output image format, added cli options
    Previous the image was:
      * black (0) on white (255)
      * 48x48 with 4 pixel margin
      * always scaled to fit 40x40 inner area
      * disproportionately scaled in x and y
    
    Updated behavior:
      * white on black
      * 64x64 with 2 pixel margin
      * only scaled down, never up
      * always scaled proportionally
      * centered
    
    Changed the output file naming scheme. Before it was simply
    tmp/image-NNNNNNN.png. Now it is split up into subdirectories
    reflecting the source file.
    
    Changed the logged output to be more informative.
    
    Added simple cli so that input directory and
    output directory could be provided.

--- code/gnt2image.py
+++ code/gnt2image.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python
+
 import os
 import glob
 import numpy, scipy.misc
 from PIL import Image
+import argparse
 
 class SingleGntImage(object):
     def __init__(self, f):
@@ -27,17 +30,40 @@ class SingleGntImage(object):
 
         return int(num_hex_str.encode('hex'), 16)
 
+    def convert_to_image(self, image_matrix, size, border):
+        coresize = size - 2*border
+
+        I = numpy.zeros((size, size), dtype=numpy.uint8)
+
+        src_image = 255-numpy.array(image_matrix, dtype=numpy.uint8)
+        src_shape = src_image.shape
+
+        if(src_shape[0] > src_shape[1]):
+            longedge = src_shape[0]
+        else:
+            longedge = src_shape[1]
+
+        scale_fraction = coresize / float(longedge)
+
+        if(scale_fraction < 1.0):
+            src_image = scipy.misc.imresize(src_image, scale_fraction)
+
+        sca_shape = src_image.shape
+        offx = int((size - sca_shape[0])/2)
+        offy = int((size - sca_shape[1])/2)
+        I[offx:offx+sca_shape[0], offy:offy+sca_shape[1]] = src_image
+
+        return I
+
     def read_single_image(self):
-        
-        # zero-one value
-        max_value = 255.0
-        margin = 4
+        margin = 2
+        size = 64
 
         # try to read next single image
         try:
             self.next_length = self.read_special_hex(4)
         except ValueError:
-            print "Notice: end of file"
+            # print "Notice: end of file"
             return None, None, None, None, True
 
         # read the chinese utf-8 label
@@ -57,36 +83,26 @@ class SingleGntImage(object):
 
             image_matrix_list.append(row)
 
-        # convert to numpy ndarray with size of 40 * 40 and add margin of 4
-        self.image_matrix_numpy = \
-            scipy.misc.imresize(numpy.array(image_matrix_list), \
-            size=(40, 40)) / max_value
-        self.image_matrix_numpy = numpy.lib.pad(self.image_matrix_numpy, \
-            margin, self.padwithones)
+        # convert to numpy ndarray with size of 64x64 and add margin of 2
+        self.image_matrix_numpy = self.convert_to_image(image_matrix_list, size, margin)
         return self.label, self.image_matrix_numpy, \
             self.width, self.height, False
-
-    def padwithones(self, vector, pad_width, iaxis, kwargs):
-        vector[:pad_width[0]] = 1.0
-        vector[-pad_width[1]:] = 1.0
-        return vector
 
 class ReadGntFile(object):
     def __init__(self):
         pass
 
-    def find_file(self):
+    def find_file(self, indir):
         file_extend = ".gnt"
         self.file_list = []
 
         # get all gnt files in the dir
-        dir_path = os.path.join(os.path.split(__file__)[0], "..", "data")
-        for file_name in glob.glob(os.path.join(dir_path, '*.gnt')):
+        for file_name in glob.glob(os.path.join(indir, '*.gnt')):
             self.file_list.append(file_name)
 
         return self.file_list
 
-    def show_image(self):
+    def show_image(self, outdir):
         count_file = 0
         count_single = 0
         width_list = []
@@ -96,7 +112,9 @@ class ReadGntFile(object):
         for file_name in self.file_list:
             count_file = count_file + 1
             end_of_image = False
+            basename = os.path.basename(file_name)[:-4]
             with open(file_name, 'rb') as f:
+                count_within_file = 0
                 while not end_of_image:
                     this_single_image = SingleGntImage(f)
 
@@ -105,29 +123,40 @@ class ReadGntFile(object):
                         this_single_image.read_single_image()
                     
                     if not end_of_image:
-                        count_single = count_single + 1
                         width_list.append(width)
                         height_list.append(height)
-                        print count_single, label, \
-                            width, height, numpy.shape(pixel_matrix)
+                        outshape = numpy.shape(pixel_matrix)
+                        print("save: {0:07d} {1:8s} {2:05d} {3} {4:03d} {5:03d} {6:03d} {7:03d}".format(count_single,
+                            basename,count_within_file, label,width, height, outshape[0], outshape[1]))
 
-                        self.save_image(pixel_matrix, label, count_single)
-                        #if count_single >= 10:
-                          #end_of_image = True
+                        self.save_image(outdir, basename, pixel_matrix, label, count_single, count_within_file)
+                        count_within_file = count_within_file + 1
+                        count_single = count_single + 1
 
-            print ("End of file #%i") % (count_file)
+            # print ("End of file #%i") % (count_file)
 
-    def save_image(self, matrix, label, count):
-        im = Image.fromarray((matrix*255).astype(numpy.uint8))
-        name = ("tmp/test-{0:07d}.png".format(count))
+    def save_image(self, outdir, basename, matrix, label, count, count_within_file):
+        outpath = os.path.join(outdir, basename)
+        if not os.path.exists(outpath):
+            os.makedirs(outpath)
+
+        im = Image.fromarray(matrix)
+        name = ("{0}/{1:05d}.png".format(outpath, count_within_file))
         im.save(name)
         # name = ("tmp/test-%s (%i).tiff") % (label, count)
         # im.save(name)
 
-def display_char_image():
+def display_char_image(indir, outdir):
     gnt_file = ReadGntFile()
-    file_list = gnt_file.find_file()
-    gnt_file.show_image()
+    file_list = gnt_file.find_file(indir)
+    gnt_file.show_image(outdir)
 
 if __name__ == '__main__':
-    display_char_image()
+    parser = argparse.ArgumentParser(
+        description="Convert dreaded gnt files to a buch of pngs")
+    parser.add_argument('--indir', dest='indir', type=str, \
+        help="Directory root for input", default="input")
+    parser.add_argument('--outdir', dest='outdir', type=str, \
+        help="Directory root for output", default="output")
+    args = parser.parse_args()
+    display_char_image(args.indir, args.outdir)
